1baf3da93408820668aa3530e8ffbc66ea2fa28a
hassnainbaloch <hassnainbaloch076@gmail.com>
2024-09-26 20:41:52 +0500

    Add files via upload

---
+++ hassnain-time/main.js
@@ -0,0 +1,48 @@
+var userinput= prompt("enter the time add in am or pm");
+if (
+    userinput === "12am"|| 
+    userinput === "1am"|| 
+    userinput === "2am"||
+    userinput === "3am"|| 
+    userinput === "4am"||
+    userinput === "5am"|| 
+    userinput === "6am"||
+    userinput === "7am"|| 
+    userinput === "8am"||
+    userinput === "9am"||
+    userinput === "10am"||
+    userinput === "11am"||
+    userinput === "12am")
+{
+    alert("good morning");
+}
+
+else if (
+    userinput === "12pm"|| 
+    userinput === "1pm"|| 
+    userinput === "2pm"||
+    userinput === "3pm"|| 
+    userinput === "4pm")
+    {
+        alert("good afternoon");
+    }
+    else if(
+        userinput === "5pm"|| 
+        userinput === "6pm"|| 
+        userinput === "7pm")
+        {
+            alert("good evening");
+        }
+        else if(
+            userinput === "8pm"|| 
+            userinput === "9pm"|| 
+            userinput === "10pm"||
+            userinput === "11pm"||
+            userinput === "12pm"   
+        )
+        {
+            alert("good night");
+        }
+    
+
+       
